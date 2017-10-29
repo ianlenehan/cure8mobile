ebb1951b4414164a050615ed3118ac79aa402b35
Ian Lenehan <ianlenehan@gmail.com>
2017-10-29 23:02:17 +1100

    Use activity data from api and start styling list

--- src/components/slides.js
+++ src/components/slides.js
@@ -1,109 +1,109 @@
-import React, { Component } from 'react';
-import { View, Text, Image, ScrollView, Dimensions, StatusBar } from 'react-native';
-import { Button } from 'react-native-elements';
-
-const SCREEN_WIDTH = Dimensions.get('window').width;
-const SCREEN_HEIGHT = Dimensions.get('window').height;
-
-const slideData = [
-  require('../../assets/images/tour_1.png'),
-  require('../../assets/images/tour_2.png'),
-  require('../../assets/images/tour_3.png'),
-  require('../../assets/images/tour_4.png'),
-  require('../../assets/images/tour_5.png')
-]
-
-class Slides extends Component {
-  renderLastSlide(index) {
-    if (index === slideData.length - 1) {
-      return (
-        <View style={styles.overlay}>
-          <Button
-          title="GOT IT!"
-          fontWeight='bold'
-          backgroundColor='#27ae60'
-          raised
-          buttonStyle={styles.buttonStyle}
-          onPress={this.props.onComplete}
-          />
-        </View>
-      );
-    }
-    return (
-      <View style={styles.overlay}>
-        <StatusBar hidden />
-        <Text style={styles.swipeRight}>Swipe right for more...</Text>
-      </View>
-    )
-  }
-
-  renderSlides() {
-    return slideData.map((slide, index) => {
-      return (
-        <View
-          key={slide}
-          style={styles.container}
-        >
-          <Image source={slide} style={styles.slide}>
-            {this.renderLastSlide(index)}
-          </Image>
-        </View>
-      );
-    });
-  }
-
-  render() {
-    return (
-      <ScrollView
-        horizontal
-        style={{ flex: 1 }}
-        pagingEnabled
-      >
-        {this.renderSlides()}
-      </ScrollView>
-    );
-  }
-}
-
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: SCREEN_WIDTH,
-    backgroundColor: '#ecf0f1'
-  },
-  slide: {
-    width: SCREEN_WIDTH * 0.90,
-    height: SCREEN_HEIGHT * 0.90,
-    marginTop: SCREEN_HEIGHT * 0.05,
-    marginBottom: SCREEN_HEIGHT * 0.05,
-    marginLeft: SCREEN_WIDTH * 0.05,
-    marginRight: SCREEN_WIDTH * 0.05,
-    borderWidth: 2,
-    borderColor: '#27ae60'
-  },
-  buttonStyle: {
-    backgroundColor: 'orange',
-    width: 250,
-    borderRadius: 5,
-    zIndex: 10,
-  },
-  overlay: {
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    flex: 1,
-    marginBottom: 30,
-    backgroundColor: 'transparent'
-  },
-  swipeRight: {
-    textShadowColor: 'black',
-    textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 4,
-    fontWeight: 'bold',
-    color: 'white',
-    fontSize: 22
-  }
-};
-
-export default Slides
+// import React, { Component } from 'react';
+// import { View, Text, Image, ScrollView, Dimensions, StatusBar } from 'react-native';
+// import { Button } from 'react-native-elements';
+//
+// const SCREEN_WIDTH = Dimensions.get('window').width;
+// const SCREEN_HEIGHT = Dimensions.get('window').height;
+//
+// const slideData = [
+//   require('../../assets/images/tour_1.png'),
+//   require('../../assets/images/tour_2.png'),
+//   require('../../assets/images/tour_3.png'),
+//   require('../../assets/images/tour_4.png'),
+//   require('../../assets/images/tour_5.png')
+// ]
+//
+// class Slides extends Component {
+//   renderLastSlide(index) {
+//     if (index === slideData.length - 1) {
+//       return (
+//         <View style={styles.overlay}>
+//           <Button
+//           title="GOT IT!"
+//           fontWeight='bold'
+//           backgroundColor='#27ae60'
+//           raised
+//           buttonStyle={styles.buttonStyle}
+//           onPress={this.props.onComplete}
+//           />
+//         </View>
+//       );
+//     }
+//     return (
+//       <View style={styles.overlay}>
+//         <StatusBar hidden />
+//         <Text style={styles.swipeRight}>Swipe right for more...</Text>
+//       </View>
+//     )
+//   }
+//
+//   renderSlides() {
+//     return slideData.map((slide, index) => {
+//       return (
+//         <View
+//           key={slide}
+//           style={styles.container}
+//         >
+//           <Image source={slide} style={styles.slide}>
+//             {this.renderLastSlide(index)}
+//           </Image>
+//         </View>
+//       );
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <ScrollView
+//         horizontal
+//         style={{ flex: 1 }}
+//         pagingEnabled
+//       >
+//         {this.renderSlides()}
+//       </ScrollView>
+//     );
+//   }
+// }
+//
+// const styles = {
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     width: SCREEN_WIDTH,
+//     backgroundColor: '#ecf0f1'
+//   },
+//   slide: {
+//     width: SCREEN_WIDTH * 0.90,
+//     height: SCREEN_HEIGHT * 0.90,
+//     marginTop: SCREEN_HEIGHT * 0.05,
+//     marginBottom: SCREEN_HEIGHT * 0.05,
+//     marginLeft: SCREEN_WIDTH * 0.05,
+//     marginRight: SCREEN_WIDTH * 0.05,
+//     borderWidth: 2,
+//     borderColor: '#27ae60'
+//   },
+//   buttonStyle: {
+//     backgroundColor: 'orange',
+//     width: 250,
+//     borderRadius: 5,
+//     zIndex: 10,
+//   },
+//   overlay: {
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//     flex: 1,
+//     marginBottom: 30,
+//     backgroundColor: 'transparent'
+//   },
+//   swipeRight: {
+//     textShadowColor: 'black',
+//     textShadowOffset: { width: 1, height: 1 },
+//     textShadowRadius: 4,
+//     fontWeight: 'bold',
+//     color: 'white',
+//     fontSize: 22
+//   }
+// };
+//
+// export default Slides
--- src/screens/activity.js
+++ src/screens/activity.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import {
   Text,
-  View
+  View,
+  ScrollView,
+  Image
 } from 'react-native'
+import { connect } from 'react-redux'
 import { Icon, Button } from 'react-native-elements'
 
-export default class Activity extends Component {
+class Activity extends Component {
   static navigationOptions = () => {
     return {
-      headerTitle: 'Activity',
+      headerTitle: 'Recent Activity',
       tabBarLabel: 'Activity',
       tabBarIcon: ({ tintColor }) => {
         return <Icon name="bell-o" type="font-awesome" size={24} color={tintColor} />;
@@ -16,16 +19,90 @@ export default class Activity extends Component {
     }
   }
 
-  render() {
+  getRatingIcon(item) {
+    if (item.rating ==='1') {
+      return (
+        <Icon
+          size={22}
+          containerStyle={{ margin: 5 }}
+          name='thumb-up'
+          color='#3498db'
+        />
+      )
+    }
     return (
-      <View>
-        <Text>Notifications Screen</Text>
-        <Text>Notifications Screen</Text>
-        <Text>Notifications Screen</Text>
-        <Text>Notifications Screen</Text>
-        <Text>Notifications Screen</Text>
-        <Text>Notifications Screen</Text>
-      </View>
+      <Icon
+        size={22}
+        containerStyle={{ margin: 5 }}
+        name='thumb-down'
+        color='#e67e22'
+      />
     )
   }
+
+  renderItems() {
+    const { activity } = this.props
+    return activity.map(item => {
+      if (item.type === 'rating') {
+        return (
+          <View key={item.id} style={styles.activityView}>
+            {this.getRatingIcon(item)}
+            <View>
+              <Text style={styles.activityTitle}>{item.title}</Text>
+              <Text>{item.friend}</Text>
+            </View>
+          </View>
+        )
+      }
+      return (
+        <View key={item.id} style={styles.activityView}>
+          <Image
+            style={{ width: 22, height: 22, borderRadius: 11, margin: 5 }}
+            resizeMode='contain'
+            source={require('../../assets/icons/app.png')}
+          />
+        <View>
+          <Text style={styles.activityTitle}>{item.title}</Text>
+          <Text>{item.friends.join(', ')}</Text>
+        </View>
+        </View>
+      )
+    })
+  }
+
+  render() {
+    if (this.props.activity.length) {
+      return (
+        <ScrollView>
+          {this.renderItems()}
+        </ScrollView>
+      )
+
+    }
+  }
 }
+
+const styles = {
+  activityView: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    marginBottom: 0.5,
+    marginTop: 0.5,
+    borderRadius: 2,
+    borderColor: '#ccc',
+    borderBottomWidth: 0,
+    padding: 3,
+    paddingRight: 10,
+    alignItems: 'center'
+  },
+  activityTitle: {
+    fontSize: 18,
+  }
+}
+
+const mapStateToProps = ({ user }) => {
+  const { activity } = user
+  return { activity }
+}
+
+export default connect(mapStateToProps, { })(Activity)
