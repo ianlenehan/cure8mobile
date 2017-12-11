a387e18cefb885a01eb8852bc51e840f989b8d68
Ian Lenehan <ian.lenehan@blake.com.au>
2017-12-11 08:54:14 +0000

    version numbers

--- environment.js
+++ environment.js
@@ -1,5 +1,5 @@
-const rootURL = 'http://localhost:3001/api/v1/';
-// const rootURL = 'https://cure8.herokuapp.com/api/v1/';
+// const rootURL = 'http://localhost:3001/api/v1/';
+const rootURL = 'https://cure8.herokuapp.com/api/v1/';
 // const rootURL = 'http://192.168.20.124:3000/api/v1/';
 // const rootURL = 'http://192.168.20.50:3000/api/v1/';
 // const rootURL = 'http://10.22.0.124:3000/api/v1/';
