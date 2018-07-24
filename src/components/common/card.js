04422038ccecea948248d6f4fa82152752e41a2e
Ian Lenehan <ian.lenehan@blake.com.au>
2018-07-24 22:19:02 +1000

    linting

--- src/components/common/card.js
+++ src/components/common/card.js
@@ -24,6 +24,93 @@ import Input from './input'
 import RatingIcons from './ratingIcons'
 import { createConversation, getConversations } from '../../redux/conversation/actions'
 
+const styles = {
+  title: {
+    fontSize: 14,
+    padding: 5,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  image: {
+    height: 200,
+    paddingBottom: 5,
+  },
+  subtitle: {
+    flexDirection: 'row',
+    margin: 5,
+  },
+  date: {
+    fontSize: 8,
+    color: 'grey',
+    flex: 1,
+    textAlign: 'right',
+  },
+  owner: {
+    fontSize: 10,
+    color: 'black',
+    textAlign: 'left',
+    paddingRight: 10,
+  },
+  count: {
+    fontSize: 10,
+    color: 'grey',
+  },
+  comment: {
+    fontSize: 12,
+    color: 'grey',
+    margin: 5,
+    flex: 4,
+  },
+  note: {
+    fontSize: 12,
+    color: 'grey',
+    margin: 5,
+  },
+  noteView: {
+    flex: 4,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flex: 1,
+    padding: 5,
+  },
+  icon: {
+    flexDirection: 'row',
+    flex: 0.5,
+    paddingRight: 10,
+    justifyContent: 'flex-end',
+  },
+  ratings: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  tagContainer: {
+    flex: 1,
+    borderTopWidth: 1,
+    borderColor: '#dcdcdc',
+    marginTop: 5,
+    paddingTop: 5,
+  },
+  smallTag: {
+    backgroundColor: 'white',
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  smallTagStyle: {
+    fontSize: 10,
+    color: '#ccc',
+  },
+  updateTagsButton: {
+    marginBottom: 10,
+    width: 150,
+    padding: 8,
+    borderRadius: 5,
+  },
+}
+
 class Card extends Component {
   constructor(props) {
     super(props)
@@ -44,13 +131,22 @@ class Card extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log('next props', nextProps.token)
     if (nextProps.tags && nextProps.tags.length) {
       const tags = nextProps.tags.sort()
       this.setState({ tags })
     }
     if (nextProps.userPhone) {
       this.setState({ phone: this.props.userPhone })
+    }
+  }
+
+  onArchivePress(owner, curation, action) {
+    if (owner.name === 'Cure8') {
+      Alert.alert('Sample Link', 'As this is a sample link, it\'s not something you can delete or archive. Add your own links and this one will go away!')
+    } else if (owner.phone === this.state.phone && action === 'deleted') {
+      this.props.justArchive(curation, 1, action)
+    } else {
+      this.props.onArchivePress(curation, action)
     }
   }
 
@@ -61,6 +157,36 @@ class Card extends Component {
     const number = Math.floor(Math.random() * (max - min)) + min
     const size = sizes[number]
     return `http://fillmurray.com/300/${size}`
+  }
+
+  openInWebBrowser = (url) => {
+    const readerMode = this.props.readerMode === 'on'
+    if (Platform.OS === 'ios') {
+      SafariView.show({
+        url,
+        readerMode,
+        tintColor: '#27ae60',
+        barColor: '#27ae60',
+      })
+    } else if (Platform.OS === 'android') {
+      CustomTabs.openURL(url)
+    }
+  }
+
+  conversationAlert() {
+    const { name: ownerName } = this.props.link.owner
+    if (this._isAlertNecessary()) {
+      Alert.alert(
+        'Chat',
+        `Select a one on one chat with ${ownerName} or start a group chat with everyone they curated this for.`,
+        [
+          { text: '1-on-1', onPress: () => this.startConversation('single') },
+          { text: 'Group', onPress: () => this.startConversation('group') },
+        ],
+      )
+    } else {
+      this.startConversation('single')
+    }
   }
 
   expandLess = () => {
@@ -86,6 +212,11 @@ class Card extends Component {
     this.props.navigate('chat', { title })
   }
 
+  formatDate(date) {
+    const currentDate = moment()
+    return moment(date).local().from(currentDate)
+  }
+
   _isAlertNecessary() {
     const { owner, shared_with: sharedWith, users_shared_with: usersSharedWith } = this.props.link
 
@@ -98,160 +229,6 @@ class Card extends Component {
     return false
   }
 
-  conversationAlert() {
-    const { name: ownerName } = this.props.link.owner
-    if (this._isAlertNecessary()) {
-      Alert.alert(
-        'Chat',
-        `Select a one on one chat with ${ownerName} or start a group chat with everyone they curated this for.`,
-        [
-          { text: '1-on-1', onPress: () => this.startConversation('single') },
-          { text: 'Group', onPress: () => this.startConversation('group') }
-        ]
-      )
-    } else {
-      this.startConversation('single')
-    }
-  }
-
-  renderConversationIcon() {
-    return (
-      <MyIcon
-        size={24}
-        name='comment'
-        type='font-awesome'
-        color="#27ae60"
-        onPress={() => this.conversationAlert()}
-        text='Discuss'
-      />
-    )
-  }
-
-  openInWebBrowser = (url) => {
-    const readerMode = this.props.readerMode === 'on'
-    if (Platform.OS === 'ios') {
-      SafariView.show({
-        url,
-        readerMode,
-        tintColor: '#27ae60',
-        barColor: '#27ae60'
-      })
-    } else if (Platform.OS === 'android') {
-      CustomTabs.openURL(url)
-    }
-  }
-
-  onArchivePress(owner, curation, action) {
-    if (owner.name === 'Cure8') {
-      Alert.alert('Sample Link', 'As this is a sample link, it\s not something you can delete or archive. Add your own links and this one will go away!')
-    } else if (owner.phone === this.state.phone && action === 'deleted') {
-      this.props.justArchive(curation, 1, action)
-    } else {
-      this.props.onArchivePress(curation, action)
-    }
-  }
-
-  formatDate(date) {
-    const currentDate = moment()
-    return moment(date).local().from(currentDate)
-  }
-
-  renderMainIcons(curation, owner, rating) {
-    if (this.props.status === 'new' && this.props.morePressed === curation) {
-      if (this.props.loading) {
-        return <Spinner size="small" />
-      }
-      return (
-        <View style={styles.icons}>
-          <MyIcon
-            size={24}
-            name='delete'
-            color="#27ae60"
-            onPress={() => this.onArchivePress(owner, curation, 'deleted')}
-            text='Delete'
-          />
-          <MyIcon
-            size={24}
-            name='archive'
-            color="#27ae60"
-            onPress={() => this.onArchivePress(owner, curation, 'archived')}
-            text='Archive'
-          />
-          {this.renderConversationIcon()}
-          <MyIcon
-            size={24}
-            type='font-awesome'
-            name='share'
-            color="#27ae60"
-            onPress={() => this.props.onSharePress(this.props.link)}
-            text='Share'
-          />
-        </View>
-      )
-    } else if (this.props.morePressed === curation) {
-      if (this.props.loading) {
-        return <Spinner size="small" />
-      }
-      return (
-        <View>
-          <View style={styles.icons}>
-            <MyIcon
-              size={24}
-              name='delete'
-              color="#27ae60"
-              onPress={() => this.props.justArchive(curation, rating, 'deleted')}
-              text='Delete'
-              />
-            {this.renderConversationIcon()}
-            <MyIcon
-              size={24}
-              type='font-awesome'
-              name='share'
-              color="#27ae60"
-              onPress={() => this.props.onSharePress(this.props.link)}
-              text='Share'
-            />
-          </View>
-          <View style={styles.tagContainer}>
-            <ScrollView
-              style={{ flex: 1 }}
-              horizontal
-            >
-              {this.renderTags()}
-            </ScrollView>
-          </View>
-          {this.addTagInput()}
-        </View>
-      )
-    }
-    return null
-  }
-
-  renderMoreIcon(curation) {
-    if (this.props.morePressed === curation) {
-      return (
-        <View style={styles.icon}>
-          <Icon
-            size={32}
-            name='expand-less'
-            color='#27ae60'
-            onPress={this.expandLess}
-          />
-        </View>
-      )
-    }
-    return (
-      <View style={styles.icon}>
-        <Icon
-          size={32}
-          name='expand-more'
-          color='#27ae60'
-          onPress={() => this.expandMore(curation)}
-        />
-      </View>
-    )
-  }
-
   toggleTag = (tag) => {
     const { selectedTags } = this.state
     if (selectedTags.includes(tag)) {
@@ -262,6 +239,78 @@ class Card extends Component {
     } else {
       this.setState({ selectedTags: [...selectedTags, tag] })
     }
+  }
+
+  addTagInput() {
+    const { archiveMode, status } = this.props
+    if (archiveMode.action === 'archived' || status === 'archived') {
+      return (
+        <View style={{ alignItems: 'center' }}>
+          <View style={{ flexDirection: 'row', margin: 10 }}>
+            <Input
+              placeholder="type to add new tag and then press '+'"
+              style={{ flex: 1, fontSize: 12, height: 30 }}
+              onChangeText={this.tagSearch}
+              value={this.state.tagSearchQuery}
+              autoCapitalize="none"
+            />
+            <Icon
+              color="#27ae60"
+              reverse
+              name="plus"
+              type="font-awesome"
+              onPress={this.addNewTag}
+              size={12}
+            />
+          </View>
+          {this.renderUpdateButton(status)}
+        </View>
+      )
+    }
+    return null
+  }
+
+  tagSearch = (query) => {
+    this.setState({ tagSearchQuery: query })
+  }
+
+  addNewTag = () => {
+    const { tags, selectedTags, tagSearchQuery } = this.state
+    const cleanTag = tagSearchQuery.toLowerCase().trim()
+    if (cleanTag.length) {
+      if (swearjar.profane(cleanTag)) {
+        Alert.alert(
+          'Oops',
+          "Let's keep this clean, we might use tags publicy in a later release.",
+          [{ text: 'Sorry' }],
+        )
+      } else if (tags.includes(cleanTag)) {
+        this.setState({ selectedTags: [...selectedTags, cleanTag] })
+      } else {
+        this.setState({
+          selectedTags: [...selectedTags, cleanTag],
+          tags: [cleanTag, ...tags],
+        })
+      }
+      this.tagSearch('')
+    }
+  }
+
+  renderUpdateButton(status) {
+    const { curation_id } = this.props.link
+    const { selectedTags } = this.state
+    if (status === 'archived') {
+      return (
+        <Button
+          title="Update tags"
+          backgroundColor="#27ae60"
+          buttonStyle={styles.updateTagsButton}
+          fontSize={12}
+          onPress={() => this.props.addTags(curation_id, selectedTags)}
+        />
+      )
+    }
+    return null
   }
 
   renderTags() {
@@ -280,87 +329,125 @@ class Card extends Component {
         )
       })
     }
+    return null
   }
 
-  renderUpdateButton(status) {
-    const { curation_id } = this.props.link
-    const { selectedTags } = this.state
-    if (status === 'archived') {
+  renderMoreIcon(curation) {
+    if (this.props.morePressed === curation) {
       return (
-        <Button
-          title='Update tags'
-          backgroundColor='#27ae60'
-          buttonStyle={styles.updateTagsButton}
-          fontSize={12}
-          onPress={() => this.props.addTags(curation_id, selectedTags)}
-        />
-      )
-    }
-  }
-
-  addTagInput() {
-    const { archiveMode, status } = this.props
-    if (archiveMode.action === 'archived' || status === 'archived') {
-      return (
-        <View style={{ alignItems: 'center' }}>
-          <View style={{ flexDirection: 'row', margin: 10 }}>
-            <Input
-              placeholder="type to add new tag and then press '+'"
-              style={{ flex: 1, fontSize: 12, height: 30 }}
-              onChangeText={this.tagSearch}
-              value={this.state.tagSearchQuery}
-              autoCapitalize={'none'}
-            />
-            <Icon
-              color='#27ae60'
-              reverse
-              name='plus'
-              type='font-awesome'
-              onPress={this.addNewTag}
-              size={12}
-            />
-          </View>
-          {this.renderUpdateButton(status)}
+        <View style={styles.icon}>
+          <Icon
+            size={32}
+            name="expand-less"
+            color="#27ae60"
+            onPress={this.expandLess}
+          />
         </View>
       )
     }
+    return (
+      <View style={styles.icon}>
+        <Icon
+          size={32}
+          name="expand-more"
+          color="#27ae60"
+          onPress={() => this.expandMore(curation)}
+        />
+      </View>
+    )
   }
 
-  tagSearch = (query) => {
-    this.setState({ tagSearchQuery: query })
-  }
-
-  addNewTag = () => {
-    const { tags, selectedTags, tagSearchQuery } = this.state
-    const cleanTag = tagSearchQuery.toLowerCase().trim()
-    if (cleanTag.length) {
-      if (swearjar.profane(cleanTag)) {
-        Alert.alert(
-          'Oops',
-          "Let's keep this clean, we might use tags publicy in a later release.",
-          [{ text: 'Sorry' }]
-        )
-      } else {
-        if (tags.includes(cleanTag)) {
-          this.setState({ selectedTags: [...selectedTags, cleanTag] })
-        } else {
-          this.setState({
-            selectedTags: [...selectedTags, cleanTag],
-            tags: [cleanTag, ...tags]
-          })
-        }
+  renderMainIcons(curation, owner, rating) {
+    if (this.props.status === 'new' && this.props.morePressed === curation) {
+      if (this.props.loading) {
+        return <Spinner size="small" />
       }
-      this.tagSearch('')
+      return (
+        <View style={styles.icons}>
+          <MyIcon
+            size={24}
+            name="delete"
+            color="#27ae60"
+            onPress={() => this.onArchivePress(owner, curation, 'deleted')}
+            text="Delete"
+          />
+          <MyIcon
+            size={24}
+            name="archive"
+            color="#27ae60"
+            onPress={() => this.onArchivePress(owner, curation, 'archived')}
+            text="Archive"
+          />
+          {this.renderConversationIcon()}
+          <MyIcon
+            size={24}
+            type="font-awesome"
+            name="share"
+            color="#27ae60"
+            onPress={() => this.props.onSharePress(this.props.link)}
+            text="Share"
+          />
+        </View>
+      )
+    } else if (this.props.morePressed === curation) {
+      if (this.props.loading) {
+        return <Spinner size="small" />
+      }
+      return (
+        <View>
+          <View style={styles.icons}>
+            <MyIcon
+              size={24}
+              name="delete"
+              color="#27ae60"
+              onPress={() => this.props.justArchive(curation, rating, 'deleted')}
+              text="Delete"
+            />
+            {this.renderConversationIcon()}
+            <MyIcon
+              size={24}
+              type="font-awesome"
+              name="share"
+              color="#27ae60"
+              onPress={() => this.props.onSharePress(this.props.link)}
+              text="Share"
+            />
+          </View>
+          <View style={styles.tagContainer}>
+            <ScrollView
+              style={{ flex: 1 }}
+              horizontal
+            >
+              {this.renderTags()}
+            </ScrollView>
+          </View>
+          {this.addTagInput()}
+        </View>
+      )
     }
+    return null
   }
 
-  renderAllIcons(owner, curation) {
+  renderConversationIcon() {
+    return (
+      <MyIcon
+        size={24}
+        name="comment"
+        type="font-awesome"
+        color="#27ae60"
+        onPress={() => this.conversationAlert()}
+        text="Discuss"
+      />
+    )
+  }
+
+  renderAllIcons(owner) {
     return owner.phone !== this.state.phone
   }
 
   renderIcons(curation, owner, rating) {
     const { archiveMode } = this.props
-    const { tags, selectedTags } = this.state
+    const { selectedTags } = this.state
     const renderAll = this.renderAllIcons(owner, curation)
     if (archiveMode.curation === curation) {
       return (
@@ -383,6 +470,7 @@ class Card extends Component {
     if (image) {
       return <Image source={{ uri: image }} style={styles.image} />
     }
+    return null
   }
 
   renderCurationTags = (tags, status) => {
@@ -394,11 +482,12 @@ class Card extends Component {
             tag={tag.name}
             style={styles.smallTag}
             tagStyle={styles.smallTagStyle}
-            onPress={console.log}
+            onPress={() => {}}
           />
         )
       })
     }
+    return null
   }
 
   render() {
@@ -413,7 +502,7 @@ class Card extends Component {
       shared_with: sharedWith,
       rating,
       tags,
-      status
+      status,
     } = this.props.link
 
     const formattedComment = comment ? `"${comment}"` : ''
@@ -422,13 +511,13 @@ class Card extends Component {
     return (
       <CardSection style={{ flex: 1 }}>
         <TouchableOpacity key={id} onPress={() => this.openInWebBrowser(url)}>
-          <Title title={title.trim()} size='small' />
+          <Title title={title.trim()} size="small" />
           <Image source={{ uri: image }} style={styles.image} />
         </TouchableOpacity>
         <View style={styles.subtitle}>
           <View style={{ flexDirection: 'row', flex: 1 }}>
             <Text style={styles.owner}>{owner.name}</Text>
-            <Icon name='people' size={14} color="grey" />
+            <Icon name="people" size={14} color="grey" />
             <Text style={styles.count}> {sharedWith}</Text>
           </View>
           <Text style={styles.date}>{this.formatDate(date)}</Text>
@@ -440,100 +529,13 @@ class Card extends Component {
           <Text style={styles.comment}>{formattedComment}</Text>
           {this.renderMoreIcon(curation)}
         </View>
-          {this.renderIcons(curation, owner, rating)}
+        {this.renderIcons(curation, owner, rating)}
       </CardSection>
     )
   }
 }
 
-const styles = {
-  title: {
-    fontSize: 14,
-    padding: 5,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  image: {
-    height: 200,
-    paddingBottom: 5
-  },
-  subtitle: {
-    flexDirection: 'row',
-    margin: 5,
-  },
-  date: {
-    fontSize: 8,
-    color: 'grey',
-    flex: 1,
-    textAlign: 'right'
-  },
-  owner: {
-    fontSize: 10,
-    color: 'black',
-    textAlign: 'left',
-    paddingRight: 10
-  },
-  count: {
-    fontSize: 10,
-    color: 'grey'
-  },
-  comment: {
-    fontSize: 12,
-    color: 'grey',
-    margin: 5,
-    flex: 4
-  },
-  note: {
-    fontSize: 12,
-    color: 'grey',
-    margin: 5,
-  },
-  noteView: {
-    flex: 4,
-    alignItems: 'center',
-    flexDirection: 'row'
-  },
-  icons: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    flex: 1,
-    padding: 5
-  },
-  icon: {
-    flexDirection: 'row',
-    flex: 0.5,
-    paddingRight: 10,
-    justifyContent: 'flex-end'
-  },
-  ratings: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  tagContainer: {
-    flex: 1,
-    borderTopWidth: 1,
-    borderColor: '#dcdcdc',
-    marginTop: 5,
-    paddingTop: 5
-  },
-  smallTag: {
-    backgroundColor: 'white',
-    borderColor: '#ccc',
-    borderWidth: 1,
-  },
-  smallTagStyle: {
-    fontSize: 10,
-    color: '#ccc',
-  },
-  updateTagsButton: {
-    marginBottom: 10,
-    width: 150,
-    padding: 8,
-    borderRadius: 5,
-  }
-}
-
-const mapStateToProps = (state) => {
+const mapStateToProps = () => {
   return {}
 }
 
